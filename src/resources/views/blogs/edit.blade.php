e2e83c482d61ef04b6900a20de97a083efefe669
Yassine Khachlek <yassine.khachlek@gmail.com>
2017-04-02 12:20:26 +0100

    added tinymce editor

--- src/resources/views/blogs/edit.blade.php
+++ src/resources/views/blogs/edit.blade.php
@@ -59,5 +59,22 @@
 @append
 
 @section('scripts')
+<script src="{{ asset('/vendor/yk/laravel-blogs/tinymce/tinymce.min.js') }}"></script>
 
+<script type="text/javascript">
+$( document ).ready(function() {
+	tinymce.init({
+	  selector: 'textarea',
+	  height: 300,
+	  menubar: true,
+	  plugins: [
+	    'advlist autolink lists link image charmap print preview anchor',
+	    'searchreplace visualblocks code fullscreen',
+	    'insertdatetime media table contextmenu paste code'
+	  ],
+	  toolbar: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+	  content_css: '//www.tinymce.com/css/codepen.min.css'
+	});
+})
+</script>
 @append
--- src/resources/views/blogs/show.blade.php
+++ src/resources/views/blogs/show.blade.php
@@ -11,7 +11,7 @@
 	</div>
 </div>
 
-<h1>{{ $blog->title }}<h1>
+<h1>{{ $blog->title }}</h1>
 
 {!! $blog->body !!}
 
